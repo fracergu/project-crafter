857e5aa5da179e26ac2a490f47e69f1cb2cdef2a
Francisco Cerdán <fcerdanguerrero@gmail.com>
2023-11-18 12:34:07 +0100

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "project-crafter.createProject",
+    async () => {
+      const technology = await vscode.window.showQuickPick(["Node", "Rust"], {
+        placeHolder: "Select the technology for your project",
+      });
+
+      if (technology === "Node") {
+        const nodeFramework = await vscode.window.showQuickPick(
+          ["React", "Angular", "Vue", "..."],
+          {
+            placeHolder: "Choose a Node.js framework",
+          }
+        );
+
+        // Here you can continue with the logic depending on the nodeFramework selection
+      }
+      // Implement similar logic for Rust if necessary
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
